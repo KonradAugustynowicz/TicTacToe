bbf38bf1bed2b202c2cec1f435d84df68ce4c3e8
guustek <guustek@wp.pl>
2022-01-25 16:52:11 +0100

    rysuje znaczki

---
+++ src/main/java/field/IField.java
@@ -0,0 +1,11 @@
+package field;
+
+import Podjebane.FieldType;
+
+import java.awt.*;
+
+public interface IField {
+    void draw(Graphics g, int x, int y, int tilesize);
+
+    FieldType getType();
+}
